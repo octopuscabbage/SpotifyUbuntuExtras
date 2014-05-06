a5d6ade498256893b3cb7c198ac256f79ce20c7f
cgrimal <clement.grimal@gmail.com>
2014-05-06 10:01:50 +0200

    track artwork instead of Spotify logo

--- spotify_notifications.py
+++ spotify_notifications.py
@@ -7,17 +7,28 @@ update_time  = 3 # The time to wait between checks (in seconds)
 expire_time  = 4000 # The expiration time of the notifications (in milliseconds)
 logo_abspath = 'spotify_80.png' # The path to a logo that will be displayed in the notification
 
-logo_abspath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/' + logo_abspath
+# logo_abspath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/' + logo_abspath
+thumb_path    = '/tmp/spotify_notification_thumb'
+wget_log_path = '/tmp/spotify_notification_wget.log'
 
 global current_track
 current_track = ""
 
+global current_thumb
+current_thumb = ""
+
 def update():
+    global current_thumb
     bus = dbus.SessionBus()
     player = bus.get_object('com.spotify.qt', '/')
     iface = dbus.Interface(player, 'org.freedesktop.MediaPlayer2')
     info = iface.GetMetadata()
+
     try:
+        thumb_url = str(info['mpris:artUrl'])
+        if current_thumb != thumb_url:
+            current_thumb = thumb_url
+            subprocess.call(['wget', '-O', thumb_path, '-o', wget_log_path, thumb_url])
         return "Title: \t\t" + str(info['xesam:title'])+"\nArtist: \t\t" + str(info['xesam:artist'][0]) + "\nAlbum: \t" +  str(info['xesam:album'])
     except KeyError: #Sometimes songs in other languages screw up
         return ""
@@ -27,12 +38,9 @@ def notify():
     track_now = update()
     if track_now != current_track:
         current_track = track_now
-        subprocess.Popen(['notify-send', '--expire-time=' + str(expire_time), '--icon=' + logo_abspath, update()])
+        subprocess.call(['notify-send', '--expire-time=' + str(expire_time), '--icon=' + thumb_path, track_now])
 
-while True:
-    notify()
-    time.sleep(update_time)
-    
-
-
-
+if __name__ == '__main__' :
+    while True:
+        notify()
+        time.sleep(update_time)
